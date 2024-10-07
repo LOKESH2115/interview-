5759d44ae765a4d2b70def4c04e17d838cbe8aea
LOKESH2115 <lokesha2115@gmail.com>
2024-10-07 15:25:21 +0530

    new file

---
+++ Class1/src/Task.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+
+export const Task = () => {
+  return (
+    <>
+    <div className='main'>
+      <navbar className="nav">
+        <h4>Innerpece</h4>
+        <div className='nav1'>
+            <h6>Home</h6>
+            <h6>Destinations</h6>
+            <h6>About</h6>
+            <h6>Contact us</h6>
+            <h6>Community</h6>
+            <button className='btn'>Book Now</button>
+            
+        </div>
+            
+      </navbar>
+      <img src='./imgnav.png ' className='img1' className="imgagemain"></img>
+      <navinfo className="navinfo">
+            
+        
+            <div className='container'>
+            <h6 > <img src='./imgnav1.png' alt=''></img>Information</h6>
+            <h6><img src='./imgnav2.png' alt=''></img>Tour Planing</h6>
+            <h6><img src='./imgnav3.png' alt=''></img>Location</h6>
+            <h6><img src='./imgnav4.png' alt=''></img>Review</h6>
+            <h6><img src='./imgnav4.png' alt=''></img>Shot Gallery</h6>
+            </div>
+           
+      </navinfo>
+    </div>
+    <div>
+        <button className='btn1'>Featured</button>
+    </div>
+    <h2>pink palace</h2>
+    <div className='rimg'>
+        <img src='./imgr1.png' alt=''></img>
+        <img src='./imgr2.png' alt=''></img>
+        <img src='./imgr3.png' alt=''></img>
+
+    </div>
+    <div>
+        <h3>
+            property Highlights
+        </h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, voluptatibus eligendi reiciendis architecto vel rem voluptas, atque eum possimus obcaecati quisquam omnis repudiandae maiores magnam nulla placeat consequuntur accusamus incidunt tempora harum accusantium esse delectus? Dolorem dicta non velit sapiente expedita accusamus, voluptatem, unde asperiores, explicabo sit fugit neque ullam ut praesentium modi labore placeat ad deleniti fugiat natus! Enim, corporis molestias rem tempore tenetur distinctio eos quos quam dolore. Quisquam ratione laborum, cupiditate veritatis officiis perspiciatis et facere molestias delectus reiciendis saepe tempore cum doloremque aut dicta velit esse est sit fugiat nemo quasi quidem. Quibusdam accusamus animi beatae!
+        </p>
+    </div>
+    <div className='box1'>
+        <h3>Amenities</h3>
+        <div className='boxx11'>
+            <p>safe parking area</p>
+            <p>pet friendly</p>
+            <p>common plug point</p>
+            <p>phone network</p>
+            <p>drinking water</p>
+        </div>    
+    </div>
+
+<div className='box1'>
+        <h3>Food and Beverages</h3>
+        <div className='boxx11'>
+            <p>breakfast included</p>
+            <p>bbq marination</p>
+            <p>camping stove@560</p>
+            <p>tea</p>
+            <p>snacks</p>
+        </div>    
+    </div>
+ <h3>Activites</h3>
+<div className='actmain'>
+    <div className='act'>
+        <p>fishing</p>
+    </div>
+    <div className='act'>
+        <p>pet visit</p>
+    </div>
+    <div className='act'>
+        <p>bird watching</p>
+    </div>
+    <div className='act'>
+        <p>cycling</p>
+    </div>
+</div>
+<div className='box1'>
+        <h3>safety Features</h3>
+        <div className='boxx11'>
+            <p>sterilized tents</p>
+            <p>backup container</p>
+            <p>safety gear</p>
+            <p>certified instructors</p>
+            <p>crowd free campsite</p>
+        </div>    
+</div>
+<h3>camp Rules</h3>
+<div>
+    <ol type='1'>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+    </ol>
+</div>
+ <h3>Important Info</h3>
+ <div>
+    <p>check-in- 2.30pm</p>
+    <p>check-out- 9.30am</p>
+    <h6>Accessible By</h6>
+    <p>bikes,cars and public transport</p>
+
+
+ </div>
+ <div>
+    <h3>cancelation policy</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore nesciunt ipsum nihil consequatur, dolores culpa non fuga delectus eius tempora magnam cupiditate vitae id dicta labore, quo assumenda exercitationem quam. Praesentium doloremque officia dolores repudiandae doloribus omnis in perferendis debitis architecto nemo? Sapiente, corrupti. Sed quia perferendis eveniet praesentium.</p>
+ </div>
+
+    </>
+  )
+}
